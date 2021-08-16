6d9509e075ab9ac31faa782bd9b352ba9f76f20c
Platon Efimov <platon.efimov@gmail.com>
2021-08-16 07:44:54 +0700

    Help pages

--- bot/src/constants/index.ts
+++ bot/src/constants/index.ts
@@ -18,3 +18,9 @@ export enum HelpStats {
   CD = 'cd',
   HEAL = 'heal',
 }
+
+export enum Contants {
+  DISCORD_URL = 'https://discord.gg/2rvxaQWj',
+  GITHUB_URL = 'https://github.com/SemperPeritus/Genshin-Artifact-Rater',
+  IMAGE_SAMPLE_URL = 'https://cdn.discordapp.com/attachments/787533173004173343/790751503475802122/unknown.png',
+}
--- bot/src/localization/en.ts
+++ bot/src/localization/en.ts
@@ -1,3 +1,5 @@
+import { Contants, Language } from '../constants';
+
 export const translationEnglish = {
   commandNotFound: 'Command not found',
   wrongParams: 'Wrong params',
@@ -8,15 +10,6 @@ export const translationEnglish = {
   disabled: 'disabled',
   disabled_plural: 'disabled',
   notAdminError: 'Administrator rights required',
-  help: {
-    general: 'help text',
-    title: 'Help',
-    sectionList: 'List of help sections',
-    config: 'TBD: help config',
-    lang: 'TBD: help lang',
-    preset: 'TBD: help preset',
-    notFound: 'Section in help pages not found',
-  },
   lisa: {
     listening: 'Listening',
   },
@@ -59,5 +52,64 @@ export const translationEnglish = {
   external: {
     notAvailable: 'This command not available for some reason',
     processingError: 'Error while processing external command',
+  },
+  helpBlock: {
+    stats:
+      '`stat` can be one of `hp`, `hp%`, `def`, `def%`, `atk`, `atk%`, `er` (Energy Recharge), ' +
+      '`em` (Elemental Mastery), `phys` (Physical DMG), `elem` (Elemental DMG), `cr` (Crit Rate), ' +
+      '`cd` (Crit Damage), `heal` (Healing Bonus).',
+  },
+  help: {
+    title: 'Help',
+    sectionList: 'List of help sections',
+    notFound: 'Section in help pages not found',
+    general: '`{{p}}help <helpSection>`',
+    config: `
+      \`{{p}}config [init/scan/prefix/mainChannel/channel]\`
+      \`{{p}}config prefix <prefixSymbol>\`
+      \`{{p}}config mainChannel <channelCode>\`
+      \`{{p}}config channel [add/rm/all/none] <channelCode>\``,
+    lang: `
+      \`{{p}}lang <langCode> [server/rater/serverRater]\`
+      List of available language codes: \`${Object.values(Language).join(', ')}\``,
+    preset: `
+      \`{{p}}preset [list/myList/serverList/add/serverAdd/rm/serverRm] <name> <...weights>\`,
+      Create a preset called \`name\` to use when rating artifacts.
+      If you want to check multiple artifacts with the same set of weights, you can use this command to create\
+      a preset with the desired weights.
+      \`weights\` will be used in the \`{{p}}rate\` command when the preset is used. \`weights\` should be in\
+      the format \`<stat>=<value>\`, where \`value\` is a number between 0 and 1.
+      {{helpBlock.stats}}
+      **Example**
+      \`{{p}}user preset healer hp=0.5 hp%=1 atk%=0\`
+      \`{{p}}rate <image> healer\`
+      \`{{p}}[user/server] preset delete <names>\`
+      Delete the presets in \`names\` (separated by spaces).`,
+    rate: `
+      \`{{p}}rate <image/url> [preset] [lvl=<level>] <...weights>\`
+      Rate an artifact against an optimal 5* artifact. Put the command and image in the same message.\
+      Try to use a clear screenshot for the best results.
+      If you are on Windows 10, you can use Shift Windows S, drag your cursor over the artifact stats and then paste it\
+      on discord with Ctrl V.
+      This bot will use default weights (see below) unless you specify your own or select a preset.\
+      You can also specify the level you want to compare your artifact to.
+      **Default weights**
+      ATK%, DMG%, Crit - 1
+      ATK, EM, Recharge – 0.5
+      Everything else - 0
+      **Parameters**
+      \`image/url\`
+      The image to be rated, either attached as a file or by putting the url in the message.\
+      [Sample](${Contants.IMAGE_SAMPLE_URL})
+      \`preset\`
+      The preset selection of weights to use. See \`{{p}}presets\` for which presets are available, or \`{{p}}help\` for how to set your own.
+      \`lvl\`
+      The level of the artifact to compare against, from 0 to 20. Sometimes the auto-detection for level is wrong, use this to correct it.
+      \`weights\`
+      The weights to use for rating this artifact. Each weight is of the format \`<stat>=<value>\`, where \`value\` is a number between 0 and 1.
+      {{helpBlock.stats}}
+      **Examples**
+      \`{{p}}rate <image> atk%=0 hp=1 er=0.5\`
+      \`{{p}}rate <url> support lvl=4\``,
   },
 };
