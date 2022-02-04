5163c7df593d69786164244495bad082af84e075
Platon Efimov <platon.efimov@gmail.com>
2022-02-04 21:03:38 +0700

    Redis multiarch

--- redis/start-redis.sh
+++ redis/start-redis.sh
@@ -3,7 +3,7 @@
 echo "Redis start script"
 
 echo "Modules installed:"
-ls -al /usr/lib/redis/modules
+ls -l /usr/lib/redis/modules
 
 # Place password from environment variable to config if not exists
 if grep -Fq "requirepass " /usr/local/etc/redis/redis.conf
