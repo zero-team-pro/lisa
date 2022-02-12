befa6f0ef987559d91158fb799c045a9cf22d991
Platon Efimov <platon.efimov@gmail.com>
2022-02-12 10:00:23 +0700

    Gateway API for guild list from shards

--- bot/src/gateway.ts
+++ bot/src/gateway.ts
@@ -14,7 +14,7 @@ require('dotenv').config();
 const { DISCORD_TOKEN, DB_FORCE, REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD, RABBITMQ_URI, SHARD_COUNT } =
   process.env;
 
-const gateway = new Bridge('gateway', {
+const bridge = new Bridge('gateway', {
   url: RABBITMQ_URI,
   shardCount: Number.parseInt(SHARD_COUNT),
   discordToken: DISCORD_TOKEN,
@@ -70,13 +70,15 @@ const databasesInit = async () => {
   }
 };
 
-const initList = [gateway.init(), databasesInit()];
+const initList = [bridge.init(), databasesInit()];
 
 /* API Express */
 
+console.log('API initialisation...');
+
 const app = express();
 
-console.log('API initialisation...');
+app.set('bridge', bridge);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -108,7 +110,7 @@ app.use((err, req, res, next) => {
 });
 
 Promise.all(initList).then(() => {
-  gateway.receiveMessages(() => {}, 'alive');
+  bridge.receiveMessages(() => {});
   app.listen(80, () => {
     console.info('Running API on port 80');
   });
