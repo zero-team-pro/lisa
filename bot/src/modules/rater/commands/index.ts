c44f68ef3a4e08f94d631ab5be55df13e7686a16
Platon Efimov <platon.efimov@gmail.com>
2022-02-15 20:42:43 +0700

    Rater commands to rater module

--- bot/src/modules/rater/commands/index.ts
+++ bot/src/modules/rater/commands/index.ts
@@ -1,7 +1,28 @@
 import { CommandMap } from '../../../types';
+import { processRaterCommand } from './rater';
+import { preset } from './preset';
+import { info } from './info';
+import { raterEngine } from './raterEngine';
 
-const commands = {};
+const commands = { processRaterCommand, preset, info, raterEngine };
 
-const commandMap: CommandMap[] = [];
+const commandMap: CommandMap[] = [
+  {
+    test: 'rate',
+    exec: commands.processRaterCommand,
+  },
+  {
+    test: 'preset',
+    exec: commands.preset,
+  },
+  {
+    test: 'info',
+    exec: commands.info,
+  },
+  {
+    test: 'raterEngine'.toLocaleLowerCase(),
+    exec: commands.raterEngine,
+  },
+];
 
 export { commandMap };
