986921fd658eb68c2c8230cd62202c8458de9d33
Platon Efimov <platon.efimov@gmail.com>
2022-03-31 00:08:03 +0700

    Admin. MUI dark theme and module filter by transport

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+
+import './index.css';
 
 import App from 'App/features/App';
 import reportWebVitals from 'App/reportWebVitals';
 import store from 'App/redux';
-
-import './index.css';
+import { theme } from 'App/theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
