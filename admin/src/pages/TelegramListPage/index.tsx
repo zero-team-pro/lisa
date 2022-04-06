db28fa340ecdf7ee9b0b29c513331f4610ad0567
Platon Efimov <platon.efimov@gmail.com>
2022-04-07 06:46:18 +0700

    Admin. Modal and Code components. Minimal Telegram CMS how-to

--- admin/src/pages/TelegramListPage/index.tsx
+++ admin/src/pages/TelegramListPage/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styles from './styles.scss';
 
+import TelegramAdd from 'App/features/TelegramAdd';
+
 const cx = require('classnames/bind').bind(styles);
 
 function TelegramListPage() {
@@ -9,6 +11,7 @@ function TelegramListPage() {
     <div className={cx('telegram-list-page')}>
       <div>
         <h1>Your Telegram Channels</h1>
+        <TelegramAdd />
       </div>
     </div>
   );
