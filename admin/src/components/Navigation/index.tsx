37b0cf6b8957d7139ea3d3a114ea21e7799fd1da
Platon Efimov <platon.efimov@gmail.com>
2022-04-07 10:35:59 +0700

    Admin. Navigation adaptive and sticky

--- admin/src/components/Navigation/index.tsx
+++ admin/src/components/Navigation/index.tsx
@@ -21,32 +21,34 @@ const defaultProps: Partial<IProps> = {
 function Navigation(props: IProps) {
   return (
     <div className={cx('navigation', { navigation_force: props.isForce })}>
-      <List>
-        <NavLink onClick={props.callback} to="/">
-          <ListItem button>
-            <ListItemIcon>
-              <HomeOutlined />
-            </ListItemIcon>
-            <ListItemText className={cx('navigation__text')} primary="Home" />
-          </ListItem>
-        </NavLink>
-        <NavLink onClick={props.callback} to="/telegram">
-          <ListItem button>
-            <ListItemIcon>
-              <TransportIcon transport={Transport.Telegram} />
-            </ListItemIcon>
-            <ListItemText className={cx('navigation__text')} primary="Telegram" />
-          </ListItem>
-        </NavLink>
-        <NavLink onClick={props.callback} to="/modules">
-          <ListItem button>
-            <ListItemIcon>
-              <ExtensionOutlined />
-            </ListItemIcon>
-            <ListItemText className={cx('navigation__text')} primary="Modules" />
-          </ListItem>
-        </NavLink>
-      </List>
+      <div className={cx('navigation__content')}>
+        <List>
+          <NavLink onClick={props.callback} to="/">
+            <ListItem button>
+              <ListItemIcon>
+                <HomeOutlined />
+              </ListItemIcon>
+              <ListItemText className={cx('navigation__text')} primary="Home" />
+            </ListItem>
+          </NavLink>
+          <NavLink onClick={props.callback} to="/telegram">
+            <ListItem button>
+              <ListItemIcon>
+                <TransportIcon transport={Transport.Telegram} />
+              </ListItemIcon>
+              <ListItemText className={cx('navigation__text')} primary="Telegram" />
+            </ListItem>
+          </NavLink>
+          <NavLink onClick={props.callback} to="/modules">
+            <ListItem button>
+              <ListItemIcon>
+                <ExtensionOutlined />
+              </ListItemIcon>
+              <ListItemText className={cx('navigation__text')} primary="Modules" />
+            </ListItem>
+          </NavLink>
+        </List>
+      </div>
     </div>
   );
 }
