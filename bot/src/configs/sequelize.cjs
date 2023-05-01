228d9a0babfe36224d32db5d7c4e1c708bf00451
Platon Efimov <platon.efimov@gmail.com>
2023-05-01 23:05:00 +0700

    Migration fix sequelize import

---
+++ bot/src/configs/sequelize.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    logging: true,
+    dialect: 'postgres',
+  },
+  production: {
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    logging: false,
+    dialect: 'postgres',
+  },
+};
