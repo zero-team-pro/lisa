0d33b0c14a0d897b7a10de5befc620d12c34f090
Platon Efimov <platon.efimov@gmail.com>
2023-06-29 20:14:53 +0700

    ls command emoji

--- bot/src/modules/core/commands/ls.ts
+++ bot/src/modules/core/commands/ls.ts
@@ -1,8 +1,30 @@
-import { CommandType } from '@/types';
+import { CommandType, Transport } from '@/types';
 import { ModuleList } from '@/modules';
 import { BaseMessage } from '@/controllers/baseMessage';
+import { MessageBuilder } from '@/controllers/messageBuilder';
 
 const methodName = 'ls';
+
+const TypeSign: Record<CommandType, string> = {
+  command: '🧰',
+  ability: '🧪',
+};
+
+const TransportSign: Record<Transport, string> = {
+  discord: '🎮',
+  telegram: '✈️',
+};
+
+const addFooter = (builder: MessageBuilder, isGlobal: boolean) => {
+  builder.addField(TypeSign.command, 'Command');
+  builder.addField(TypeSign.ability, 'Ability (not runnable by user)');
+
+  if (isGlobal) {
+    builder.addEmptyLine();
+    builder.addField(TransportSign.discord, 'Discord');
+    builder.addField(TransportSign.telegram, 'Telegram');
+  }
+};
 
 const exec = async (message: BaseMessage) => {
   const [, param] = message.content.split(' ');
@@ -11,22 +33,31 @@ const exec = async (message: BaseMessage) => {
   const builder = message.getMessageBuilder();
 
   ModuleList.map((module) => {
-    builder.addBoldLine(module.title);
-
     const moduleList = isGlobal
       ? module.commandMap
       : module.commandMap.filter((com) => com.transports.includes(message.transport));
 
+    if (moduleList.length === 0) {
+      return;
+    }
+
+    builder.addBoldLine(module.title);
+
     moduleList.forEach((com) => {
+      const transports = isGlobal ? `${com.transports.map((tr) => TransportSign[tr]).join('')}  ` : '';
+      const title = `${transports}${TypeSign[com.type]}  ${com.title}`;
+
       if (com.type === CommandType.Command && com.transports.includes(message.transport)) {
-        builder.addField(com.title, com.description);
+        builder.addField(title, com.description);
       } else {
-        builder.addFieldItalic(com.title, com.description);
+        builder.addFieldItalic(title, com.description);
       }
     });
 
     builder.addEmptyLine();
   });
+
+  addFooter(builder, isGlobal);
 
   await builder.reply();
 };
